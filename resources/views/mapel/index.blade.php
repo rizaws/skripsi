89fbe8b669a04c192ccb850b4c6fa3f7ee1ca637
nadws <nandw567@gmail.com>
2023-07-03 08:18:53 +0800

    dsa

--- resources/views/mapel/index.blade.php
+++ resources/views/mapel/index.blade.php
@@ -1,144 +1,144 @@
 @extends('theme.app')
 @section('content')
-    <div id="main">
-        <header class="mb-3">
-            <a href="#" class="burger-btn d-block d-xl-none">
-                <i class="bi bi-justify fs-3"></i>
-            </a>
-        </header>
-        <div class="page-content">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <div class="row">
-                                <div class="col-lg-6">
-                                    <h6>{{ $title }}</h6>
-                                </div>
-                                <div class="col-lg-6">
-                                    <a href="#" class="btn btn-primary float-end" data-bs-toggle="modal"
-                                        data-bs-target="#tambah"><i class="fas fa-plus"></i> Tambah
-                                        Mapel</a>
-                                </div>
+<div id="main">
+    <header class="mb-3">
+        <a href="#" class="burger-btn d-block d-xl-none">
+            <i class="bi bi-justify fs-3"></i>
+        </a>
+    </header>
+    <div class="page-content">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <h6>{{ $title }}</h6>
                             </div>
+                            <div class="col-lg-6">
+                                <a href="#" class="btn btn-primary float-end" data-bs-toggle="modal"
+                                    data-bs-target="#tambah"><i class="fas fa-plus"></i> Tambah
+                                    Mapel</a>
+                            </div>
+                        </div>
 
 
-                        </div>
-                        <div class="card-body">
-                            <table class="table table-striped" id="table1">
-                                <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Mata Pelajaran</th>
-                                        <th>Aksi</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($mapel as $no => $s)
-                                        <tr>
-                                            <td>{{ $no + 1 }}</td>
-                                            <td>{{ $s->nm_mapel }}</td>
-                                            <td>
-                                                <a href="#" data-bs-toggle="modal" data-bs-target="#edit" class="btn btn-sm btn-warning edit"
-                                                    id_mapel="{{ $s->id_mapel }}"><i class="fas fa-edit"
-                                                        ></i></a>
-                                                <a href="#" class="btn btn-sm btn-danger hapus" data-bs-toggle="modal"
-                                                    data-bs-target="#hapus" id_mapel="{{ $s->id_mapel }}"><i
-                                                        class="fas fa-trash-alt"></i></a>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-striped" id="table1">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Mata Pelajaran</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($mapel as $no => $s)
+                                <tr>
+                                    <td>{{ $no + 1 }}</td>
+                                    <td>{{ $s->nm_mapel }}</td>
+                                    <td>
+                                        <a href="#" data-bs-toggle="modal" data-bs-target="#edit"
+                                            class="btn btn-sm btn-warning edit" id_mapel="{{ $s->id_mapel }}"><i
+                                                class="fas fa-edit"></i></a>
+                                        <a href="#" class="btn btn-sm btn-danger hapus" data-bs-toggle="modal"
+                                            data-bs-target="#hapus" id_mapel="{{ $s->id_mapel }}"><i
+                                                class="fas fa-trash-alt"></i></a>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 
 
-        <form action="{{ route('tambah_mapel') }}" method="post">
-            @csrf
-            <div class="modal fade text-left" id="tambah" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1"
-                aria-hidden="true">
-                <div class="modal-dialog " role="document">
-                    <div class="modal-content ">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="myModalLabel1">Tambah Mata Pelajaran</h5>
-                            <button type="button" class="close rounded-pill" data-bs-dismiss="modal" aria-label="Close">
-                                <i data-feather="x"></i>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <label for="">Nama Mata Pelajaran</label>
-                                    <input type="text" class="form-control" name="nm_mapel">
-                                </div>
+    <form action="{{ route('tambah_mapel') }}" method="post">
+        @csrf
+        <div class="modal fade text-left" id="tambah" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1"
+            aria-hidden="true">
+            <div class="modal-dialog " role="document">
+                <div class="modal-content ">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="myModalLabel1">Tambah Mata Pelajaran</h5>
+                        <button type="button" class="close rounded-pill" data-bs-dismiss="modal" aria-label="Close">
+                            <i data-feather="x"></i>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <label for="">Nama Mata Pelajaran</label>
+                                <input type="text" class="form-control" name="nm_mapel">
                             </div>
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn" data-bs-dismiss="modal">
-                                <i class="bx bx-x d-block d-sm-none"></i>
-                                <span class="d-none d-sm-block">Tutup</span>
-                            </button>
-                            <button type="submit" class="btn btn-primary">Simpan</button>
-                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn" data-bs-dismiss="modal">
+                            <i class="bx bx-x d-block d-sm-none"></i>
+                            <span class="d-none d-sm-block">Tutup</span>
+                        </button>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
                     </div>
                 </div>
             </div>
-        </form>
+        </div>
+    </form>
 
-        <form action="{{ route('edit_mapel') }}" method="post">
-            @csrf
-            <div class="modal fade text-left" id="edit" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1"
-                aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content ">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="myModalLabel1">Edit Mata Pelajaran</h5>
-                            <button type="button" class="close rounded-pill" data-bs-dismiss="modal" aria-label="Close">
-                                <i data-feather="x"></i>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <div id="mapel"></div>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn" data-bs-dismiss="modal">
-                                <i class="bx bx-x d-block d-sm-none"></i>
-                                <span class="d-none d-sm-block">Tutup</span>
-                            </button>
-                            <button type="submit" class="btn btn-primary">Simpan</button>
-                        </div>
+    <form action="{{ route('edit_mapel') }}" method="post">
+        @csrf
+        <div class="modal fade text-left" id="edit" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1"
+            aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content ">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="myModalLabel1">Edit Mata Pelajaran</h5>
+                        <button type="button" class="close rounded-pill" data-bs-dismiss="modal" aria-label="Close">
+                            <i data-feather="x"></i>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div id="mapel"></div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn" data-bs-dismiss="modal">
+                            <i class="bx bx-x d-block d-sm-none"></i>
+                            <span class="d-none d-sm-block">Tutup</span>
+                        </button>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
                     </div>
                 </div>
             </div>
-        </form>
+        </div>
+    </form>
 
 
-        <form method="get" action="{{ route('delete_mapel') }}">
-            <div class="modal fade" id="hapus" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content">
-                        <div class="modal-body">
-                            <div class="row">
-                                <h5 class="text-danger ms-4 mt-4"><i class="fas fa-trash"></i> Hapus Data</h5>
-                                <p class=" ms-4 mt-4">Apa anda yakin ingin menghapus ?</p>
-                                <input type="hidden" class="id_mapel" name="id_mapel">
-                            </div>
+    <form method="get" action="{{ route('delete_mapel') }}">
+        <div class="modal fade" id="hapus" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <div class="row">
+                            <h5 class="text-danger ms-4 mt-4"><i class="fas fa-trash"></i> Hapus Data</h5>
+                            <p class=" ms-4 mt-4">Apa anda yakin ingin menghapus ?</p>
+                            <input type="hidden" class="id_mapel" name="id_mapel">
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Batal</button>
-                            <button type="submit" class="btn btn-danger">Hapus</button>
-                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Batal</button>
+                        <button type="submit" class="btn btn-danger">Hapus</button>
                     </div>
                 </div>
             </div>
-        </form>
+        </div>
+    </form>
 
 
-        {{-- <footer>
+    {{-- <footer>
         <div class="footer clearfix mb-0 text-muted">
             <div class="float-start">
                 <p>2023 &copy; Pengadilan Negeri Banjarmasin</p>
@@ -149,11 +149,11 @@
             </div>
         </div>
     </footer> --}}
-    </div>
+</div>
 @endsection
 @section('scripts')
-    <script>
-        $(document).ready(function() {
+<script>
+    $(document).ready(function() {
             $(document).on('click', '.edit', function() {
                 var id_mapel = $(this).attr('id_mapel');
 
@@ -168,8 +168,8 @@
             $(document).on('click', '.hapus', function() {
                 var id_mapel = $(this).attr('id_mapel');
 
-                $('.id_mapel').val(id_kelas);
+                $('.id_mapel').val(id_mapel);
             });
         });
-    </script>
+</script>
 @endsection
