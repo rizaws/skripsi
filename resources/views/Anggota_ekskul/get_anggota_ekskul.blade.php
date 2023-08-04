ca088fe948906f9d96993e97168e31ff39c19d48
nadws <nandw567@gmail.com>
2023-08-04 08:10:32 +0800

    dsa

--- resources/views/Anggota_ekskul/get_anggota_ekskul.blade.php
+++ resources/views/Anggota_ekskul/get_anggota_ekskul.blade.php
@@ -4,9 +4,9 @@
         <select name="" id="" class="choices form-select get_siswa">
             <option value="">-Pilih Kelas-</option>
             @foreach ($kelas as $k)
-                <option value="{{ $k->id_kelas }}" {{ $k->id_kelas == $id_kelas ? 'Selected' : '' }}>
-                    {{ $k->kelas }}{{ $k->huruf }}
-                </option>
+            <option value="{{ $k->id_kelas }}" {{ $k->id_kelas == $id_kelas ? 'Selected' : '' }}>
+                {{ $k->kelas }}{{ $k->huruf }}
+            </option>
             @endforeach
         </select>
         <input type="hidden" name="id_anggota_ekskul" value="{{ $anggota->id_anggota_ekskul }}">
@@ -15,9 +15,9 @@
         <label for="">Pilih Siswa</label>
         <select name="id_siswa" id="" class="form-control load_siswa">
             @foreach ($siswa as $s)
-                <option value="{{ $s->id_siswa }}" {{ $prestasi->id_siswa == $s->id_siswa ? 'Selected' : '' }}>
-                    {{ $s->nama }}
-                </option>
+            <option value="{{ $s->id_siswa }}" {{ $prestasi->id_siswa == $s->id_siswa ? 'Selected' : '' }}>
+                {{ $s->nama }}
+            </option>
             @endforeach
         </select>
     </div>
@@ -26,8 +26,8 @@
         <select name="id_ekskul" id="" class="choices form-select">
             <option value="">-Pilih Ekskul-</option>
             @foreach ($ekskul as $e)
-                <option value="{{ $e->id_ekskul }}" {{ $anggota->id_ekskul == $e->id_ekskul ? 'Selected' : '' }}>
-                    {{ $e->nm_ekskul }}</option>
+            <option value="{{ $e->id_ekskul }}" {{ $anggota->id_ekskul == $e->id_ekskul ? 'Selected' : '' }}>
+                {{ $e->nm_ekskul }}</option>
             @endforeach
         </select>
 
